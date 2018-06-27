4b3fd84803bd34a8c23d38deafc5a642f1847bcd
shreejan <shreejans@lexi.co.jp>
2018-06-27 10:14:46 +0900

    Test projects: extrude_sphere and implicit_dataset.

---
+++ extrude_sphere/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <vtkOBJReader.h>
+#include <vtkLinearExtrusionFilter.h>
+#include <vtkSTLWriter.h>
+
+using namespace std;
+
+int main()
+{
+	auto objfile = string(R"(D:\projects\ZV_WORK\src\work\Production\msvs\Zed\subPoly.obj)");
+	//auto objfile = string(R"(d:\sphere.obj)");
+
+	auto objreader = vtkSmartPointer<vtkOBJReader>::New();
+	objreader->SetFileName(objfile.c_str());
+	objreader->Update();
+
+	auto objpoly = objreader->GetOutput();
+
+	auto linearextrusion = vtkSmartPointer<vtkLinearExtrusionFilter>::New();
+	linearextrusion->SetInputData(objpoly);
+	linearextrusion->SetCapping(1);
+	linearextrusion->SetExtrusionTypeToPointExtrusion();
+	//linearextrusion->SetExtrusionPoint(0., 0., 0.);
+	linearextrusion->SetExtrusionPoint(91.350445360311966, 165.46352360347873, -93.616526598563397);
+	//linearextrusion->SetScaleFactor(-1. * .1);
+	linearextrusion->SetScaleFactor(.1);
+	linearextrusion->Update();
+
+	auto outputlinearextrusion = linearextrusion->GetOutput();
+
+	auto stlwriter = vtkSmartPointer<vtkSTLWriter>::New();
+	stlwriter->SetFileName(R"(d:\linextsphere.stl)");
+	stlwriter->SetInputData(outputlinearextrusion);
+	stlwriter->Update();
+
+	return 0;
+}
+
---
+++ implicit_dataset/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <vtkOBJReader.h>
+#include <vtkImplicitPolyDataDistance.h>
+
+using namespace std;
+
+int main()
+{
+	auto objFile = string(R"(d:\sphere.obj)");
+
+	auto objReader = vtkSmartPointer<vtkOBJReader>::New();
+	objReader->SetFileName(objFile.c_str());
+	objReader->Update();
+
+	auto outObjReader = objReader->GetOutput();
+
+	auto implicitPolyDataDistance = vtkSmartPointer<vtkImplicitPolyDataDistance>::New();
+	implicitPolyDataDistance->SetInput(outObjReader);
+
+	for(int i = 0; i < 291600; ++i)
+	{
+		auto v0 = implicitPolyDataDistance->FunctionValue(.0, .0, .0);
+	}
+
+	return 0;
+}
